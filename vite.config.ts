6735b38efa17fcbc2e7a4874a665e91bf14c93b8
mbudit <buditriono13@gmail.com>
2026-02-16 12:10:51 +0700

    fix: added the ability to see manifests on dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig({
     }),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'Money Manager',
